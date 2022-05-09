3786d670a2c7b97d6e4e12ef4cf0be343b2c8e01
hjs926 <joonsun926@gmail.com>
2022-05-09 22:36:41 +0900

    Update pr1.py

--- practice/pr1.py
+++ practice/pr1.py
@@ -1,2 +1,34 @@
 # 인덱싱 a="abcd e" a[2]=c a[5]=e 
 # 슬라이싱 [이상:미만:간격] b[::-2] 뒤로두칸
+#포멧팅
+#a = "my age is {age}".format(age="27")
+#print(a)
+
+#b = "%.4f" % 3.141592
+#print(b)
+
+# 문자열 개수 세기(count) a="hobby" a.count('b) //2 
+# 위치 알려주기 (find)  a="abcdefg"  a.find('b') //3 없으면 -1이 나옴
+
+# 문자열 삽입
+#a="," .join("abcd")
+#print(a)
+
+#소문자 대문자 바꾸기 upper lower  a.upper() a.lower()
+
+# a.strip() 공백 제거
+
+# replace 문자열 바꾸기
+
+# apped 맨 뒤에 추가한다 insert(0,4) 0번째 인덱스에 4추가
+
+# sort 가나다 알파벳 순 숫자순
+
+# remove(1) 1이라는 값을 삭제
+
+# pop() 리스트요소 끄집어 내기
+
+# a.count(1) 1이 몇개 있는지 세준다
+
+# a.extend([2,3]) 리스트 확장 (리스트에 추가해준다)
+
