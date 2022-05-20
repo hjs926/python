ac91a09f1b409515f14a0f95266b69174e10ce18
hjs926 <joonsun926@gmail.com>
2022-05-20 22:33:29 +0900

    update

--- BRONZE/10871_X보다 작은수.py
+++ BRONZE/10871_X보다 작은수.py
@@ -1,7 +1,7 @@
 # join함수, 리스트 컴프리헨션
 # print(''.join(['p','y','t','h','o','n'])) 출력 python
 # print(' '.join(map(str, [1,2,3,4,5]))) 출력 1 2 3 4 5
-# print
+# print([i for i in range(10) if i <5]) # 출력 : [0, 1, 2, 3, 4]
 
 n,x = map(int,input().split())
 a = list(map(int,input().split()))
