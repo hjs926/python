138dafbd9f275effb376f2f1720f9c8ffced3b63
hjs926 <joonsun926@gmail.com>
2022-05-21 20:30:49 +0900

    update

--- BRONZE/2675_문자열반복.py
+++ BRONZE/2675_문자열반복.py
@@ -5,3 +5,13 @@ for i in range(t):
     for i in s:
         text += int(num) * i
     print(text)
+
+
+t = int(input())
+for _ in range(t):
+    a,b = input().split()
+    a = int(a)
+    s = ''
+    for x in b:
+        s += x*a
+    print(s)
