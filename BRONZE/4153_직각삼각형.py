784219950ff3df0c588c3cff595e9b8d859507fb
hjs926 <joonsun926@gmail.com>
2022-05-23 20:40:30 +0900

    update

--- BRONZE/4153_직각삼각형.py
+++ BRONZE/4153_직각삼각형.py
@@ -1,2 +1,8 @@
 # 배열.sort(): 배열이 오름차순으로 정렬됨
 # sorted(배열): 오름차순으로 정렬된 배열을 반환함
+
+while True:
+    a,b,c = sorted(list(map(int, input().split())))
+    if not a:
+        break
+    print('right' if c**2 == a**2 + b**2 else 'wrong')
