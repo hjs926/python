88c6ac39114b29e6fb909ce36bb07cd23fcc73aa
hjs926 <joonsun926@gmail.com>
2022-05-26 20:06:09 +0900

    update

--- BRONZE/1259_펠린드롬수.py
+++ BRONZE/1259_펠린드롬수.py
@@ -2,14 +2,15 @@
 def palidrome(s):
     i,j = 0, len(s)-1
     while i<j:
-        if s[i] = s[j]:
+        if s[i] == s[j]:
             i+=1
             j-=1
         else:
             return 'no'
     return 'yes'
-    
+
 while True:
     s = input()
     if s == '0':
         break
+    print(palidrome(s))
