728cdb47d43e7470bb951c0a3be932d6061edc5c
hjs926 <joonsun926@gmail.com>
2022-06-04 13:24:25 +0900

    Update 2751_수 정렬하기 2.py

--- SILVER/2751_수 정렬하기 2.py
+++ SILVER/2751_수 정렬하기 2.py
@@ -5,10 +5,10 @@ for i in range(n):
     l.append(int(sys.stdin.readline()))
 for i in sorted(l):
     sys.stdout.write(str(i)+'\n')
+    
 # 1.  파이썬 -> 인터프리터 언어 느림
 # 1-> pypy3 이용 = 파이썬, 데이터 처리 속도 향상, 메모리 많이
 # 입출력 -> 표준 입출력
 # 3.3 퀵 정렬 -> 최악의 경우 0(n^2)
-
 # 1 2 3 4 5 6 7 8 9 10 -> 시간초과 x 메모리초과
 # -> 피벗을 랜덤
