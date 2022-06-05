6ff8fd2a4e074f35c4621ba2fb550163780d334a
hjs926 <joonsun926@gmail.com>
2022-06-05 20:48:41 +0900

    Update 10989_수 정렬하기 3.py

--- SILVER/10989_수 정렬하기 3.py
+++ SILVER/10989_수 정렬하기 3.py
@@ -1,10 +1,12 @@
-n = int(input())
+import sys
 
-sortList = [0]*10001
+n = int(sys.stdin.readline())
+num_list = [0] * 10001
 
-for i in range(n):
-    sortList[int(input())] += 1
+for _ in range(n):
+    num_list[int(sys.stdin.readline())] += 1
 
-for i in range(1, 10001):
-    for j in range(sortList[i]):
-        print(i)
+for i in range(10001):
+    if num_list[i] != 0:
+        for j in range(num_list[i]):
+            print(i)
