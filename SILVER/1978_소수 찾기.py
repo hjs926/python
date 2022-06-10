639ebda9c588d6b9fe64748c91da5d6e0592443d
hjs926 <joonsun926@gmail.com>
2022-06-10 23:07:08 +0900

    Update 1978_소수 찾기.py

--- SILVER/1978_소수 찾기.py
+++ SILVER/1978_소수 찾기.py
@@ -1,9 +1,14 @@
-sosu = 0
-for num in numbers:
-    error = 0
-    if num > 1 :
-        for i in range(2, num):  # 2부터 n-1까지
-            if num % i == 0:
-                error += 1  # 2부터 n-1까지 나눈 몫이 0이면 error가 증가
-        if error == 0:
-            sosu += 1 
+n = int(input()) # 입력받을 숫자의 개수
+nums = list(map(int, input().split(' '))) # 공백으로 숫자 구분. ex) 1 3 5 7
+resCnt = 0 # 소수의 개수
+
+for i in nums:
+    cnt = 0 
+    if(i == 1): # 1은 소수가 아니기 때문에 건너띔
+        continue
+    for j in range(2, i+1):
+        if(i % j == 0):
+            cnt += 1
+    if(cnt == 1):
+        resCnt += 1
+print(resCnt)
