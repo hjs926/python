16b7c1a3888a01b394e6d62914bf00131ea702a0
hjs926 <joonsun926@gmail.com>
2022-06-11 19:26:55 +0900

    Update 2108_통계학.py

--- SILVER/2108_통계학.py
+++ SILVER/2108_통계학.py
@@ -1,32 +1,23 @@
-n = int(input())
-arr = []
-total = 0
-count =[0]*(8001)
-mcount = 0
-_max = -40001
-_min = 400001
+import sys
+from collections import Counter
+n = int(sys.stdin.readline())
+li = []
 for _ in range(n):
-    x = int(input())
-    arr.append(x)
-    count[x] +=1
-    total +=x
-    _max = max(_max, x)
-    _min = min(_min, x)
-    mcount = max(mcount, count[x])
-avg = total / n
-arr.sort()
-mid = arr[n/2]
-cnt = 0
-mode = 0
-for i in range(-4000, 4001):
-    if mcount == count[i]:
-        mode = 1
-        cnt += 1
-        if cnt == 2:
-            break
-rng = _max - _min
-print(format(avg,'.0f'))
-print(mid)
-print(mode)
-print(rng)
-    
+    li.append(int(sys.stdin.readline()))
+ 
+# 산술평균 - 다 더해서 / n
+print(round(sum(li)/n))
+ 
+# 중앙값 - 오름차순 -> 중간값
+li.sort()
+print(li[n//2])
+ 
+# 최빈값 - 빈출
+cnt_li = Counter(li).most_common()
+if len(cnt_li) > 1 and cnt_li[0][1]==cnt_li[1][1]: #최빈값 2개 이상
+    print(cnt_li[1][0])
+else:
+    print(cnt_li[0][0])
+ 
+# 범위 - 최댓값-최솟값
+print(max(li)-min(li))
