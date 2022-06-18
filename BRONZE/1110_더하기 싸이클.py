8d556155e6338dbcd5720486b3c05074349d95d9
hjs926 <joonsun926@gmail.com>
2022-06-18 19:32:57 +0900

    Update 1110_더하기 싸이클.py

--- BRONZE/1110_더하기 싸이클.py
+++ BRONZE/1110_더하기 싸이클.py
@@ -1,14 +1,14 @@
-n = int(input())
-num =ncnt = 0
-
+N = int(input())  #입력받은 값을 int로 바꿈
+num = N           #변하는 값
+count = 0         #몇 번 사이클인지
+ 
 while True:
-    a = num //10
+    a = num//10
     b = num %10
     c = (a+b)%10
-    num=(b*10)+c
-
-    cnt = cnt+1
-    if(num==n):
+    num = (b*10) + c
+    count += 1
+    if(num == N):
         break
-
-print(cnt)
+ 
+print(count)
