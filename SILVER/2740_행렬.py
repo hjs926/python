00b5633f5b0ccf602889dbdb5cbf5faaf729fc35
hjs926 <joonsun926@gmail.com>
2022-06-27 17:52:16 +0900

    Update 2740_행렬.py

--- SILVER/2740_행렬.py
+++ SILVER/2740_행렬.py
@@ -1,2 +1,6 @@
 N, M = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(N)]
+
+M, K = map(int, input().split())
+B = [list(map(int, input().split())) for _ in range(M)]
+
