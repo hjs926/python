b365ae6f3a4fd0418814b22be2a2ee07d1415626
hjs926 <joonsun926@gmail.com>
2022-07-10 13:25:20 +0900

    Create 1193_분수찾기.py

---
+++ BRONZE/1193_분수찾기.py
@@ -0,0 +1,21 @@
+from re import X
+
+
+x=int(input())
+
+line=1
+while X>line:
+    X-=line
+    line+=1
+
+if line%2==0:
+    a=X
+
+if line%2==0:
+    a=X
+    b=line-X+1
+else:
+    a=line-X+1
+    b=X
+
+print(a, '/',b sep='')
