3b68b82ed5bde5111619eb09f70b0797fe553936
hjs926 <joonsun926@gmail.com>
2022-07-11 15:40:13 +0900

    Update 11653_소인수분해.py

--- BRONZE/11653_소인수분해.py
+++ BRONZE/11653_소인수분해.py
@@ -1,12 +1,8 @@
-n = int(input())
-
-if n == 1:
-    print('')
-
-# 2부터 하나씩 나눠보기
-for i in range(2, n+1):
-    if n % i == 0:
-    	#해당 숫자로 나눌 수 없을 때까지 나누기
-        while n % i == 0:
-            print(i)
-            n = n / i
+N = int(input())
+m = 2
+while N!=1:  # N과m을 나눴을때 몫이 1이 되면 멈춤.
+  if N%m==0: 
+    print(m) 
+    N = N//m
+  else:
+    m += 1
