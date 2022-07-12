0cf20f02b042917cfda5e8368aa5a5b8cfd9933b
hjs926 <joonsun926@gmail.com>
2022-07-12 13:19:45 +0900

    Update 24416_알고리즘 수업 - 피보나치 수 1.py

--- BRONZE/24416_알고리즘 수업 - 피보나치 수 1.py
+++ BRONZE/24416_알고리즘 수업 - 피보나치 수 1.py
@@ -4,3 +4,12 @@ def fib(n):
     else:
         return fib(n-1)+fib(n-2)
         
+def fibonacci(n):
+    dp=[0]*(n+1)
+    dp[1],dp[2]=1,1
+    cnt2=0
+    for i in range(3,n+1):
+        cnt2+=1
+        dp[i]=dp[i-1]+dp[i-2]
+    return cnt2
+
