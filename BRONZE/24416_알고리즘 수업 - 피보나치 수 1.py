7034048e57fede2ed5972bbdb3a20036bdd079b0
hjs926 <joonsun926@gmail.com>
2022-07-12 13:21:53 +0900

    Update 24416_알고리즘 수업 - 피보나치 수 1.py

--- BRONZE/24416_알고리즘 수업 - 피보나치 수 1.py
+++ BRONZE/24416_알고리즘 수업 - 피보나치 수 1.py
@@ -1,22 +1,13 @@
+f = [None]*50
+f[1] = f[2] = 1
+
 n = int(input())
-ans = [0, 1]
-dp_li = [0, 1, 1]
 
-def recur(n):
-    global ans
-    if n < 3:
-        ans[0] += 1
-        return 1
-    else: return recur(n - 1) + recur(n - 2)
+def fibonacci(n):
+    for i in range(3, n+1):
+        f[i] = f[i-1]+f[i-2]
+    return f[n]
 
-def dp(n):
-    global ans, dp_li
-    if n < 3: return dp_li[n]
-    for i in range(3, n):
-        ans[1] += 1
-        dp_li.append(dp_li[i - 1] + dp_li[i - 2])
-    return dp_li[-1]
-
-recur(n)
-dp(n)
-print(*ans)
+count1 = fibonacci(n)
+count2 = n-2
+print(k1, k2)
