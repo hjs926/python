5656091b8a65cd8a8e1211a22f2b8fe04cf409eb
hjs926 <joonsun926@gmail.com>
2022-07-19 21:37:35 +0900

    Update 9148_신나는 함수 실행.py

--- SILVER/9148_신나는 함수 실행.py
+++ SILVER/9148_신나는 함수 실행.py
@@ -14,3 +14,11 @@ def w(a, b, c):
     dp[a][b][c] = w(a-1, b, c)+w(a-1,b-1,c)+w(a-1,b,c-1)-w(a-1,b-1,c-1)
     return dp[a][b][c]
 
+dp = [[[0]*(21) for _ in range(21)] for _ in range(21)]
+# 0~20까지므로
+
+while 1:
+    a, b, c = map(int, input().split())
+    if a==-1 and b==-1 and c==-1:
+        break
+    print(f'w({a}, {b}, {c}) = {w(a,b,c)}')
