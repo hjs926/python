4322943e989f22a1ce92b24d745d17fe28fe88e5
hjs926 <joonsun926@gmail.com>
2022-07-20 21:31:37 +0900

    Update 10816_숫자카드 2.py

--- SILVER/10816_숫자카드 2.py
+++ SILVER/10816_숫자카드 2.py
@@ -1,25 +1,21 @@
-from sys import stdin
-n = stdin.readline().rstrip()
-card = list(map(int,stdin.readline().split()))
-m = stdin.readline().rstrip()
-test = list(map(int,stdin.readline().split()))
+import sys
+input = sys.stdin.readline
 
-card.sort()
+N = int(input())
+cards = [*map(int, input().split())]
+M = int(input())
+candidate = [*map(int, input().split())]
 
-def binary_search(array, target, start, end):
-    if start > end:
-        return None
-    mid = (start + end) // 2
-    if array[mid] == target:
-        return array[start:end+1].count(target)
-    elif array[mid] < target:
-        return binary_search(array, target, mid+1, end)
+count = {}
+for card in cards:
+    if card in count:
+        count[card] += 1
     else:
-        return binary_search(array, target, start, mid-1)
+        count[card] = 1
 
-for i in range(len(test)):
-    a = binary_search(card, test[i], 0, len(card)-1)
-    if a is not None:
-        print(a, end=' ')
+for target in candidate:
+    result = count.get(target)
+    if result == None:
+        print(0, end=" ")
     else:
-        print(0, end=' ')
+        print(result, end=" ")
