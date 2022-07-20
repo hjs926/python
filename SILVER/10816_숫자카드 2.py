d18408f2f0487d3f19366effca94ac57123644c0
hjs926 <joonsun926@gmail.com>
2022-07-20 21:28:46 +0900

    Update 10816_숫자카드 2.py

---
+++ SILVER/10816_숫자카드 2.py
@@ -0,0 +1,7 @@
+from sys import stdin
+n = stdin.readline().rstrip()
+card = list(map(int,stdin.readline().split()))
+m = stdin.readline().rstrip()
+test = list(map(int,stdin.readline().split()))
+
+card.sort()
