59d9c6330547117876ab59bf78d86c41235fc693
hjs926 <joonsun926@gmail.com>
2022-08-04 20:13:42 +0900

    Update 2559_수열.py

--- SILVER/2559_수열.py
+++ SILVER/2559_수열.py
@@ -1,11 +1,11 @@
 N, K = map(int, input().split())
 tem_list = list(map(int, input().split()))
 
-result_list = []
-for i in range(len(tem_list)):
-    if i + K - 1 == len(tem_list) - 1:
-        result_list.append(sum(tem_list[i:i+K]))
-        break
-    result_list.append(sum(tem_list[i:i+K]))
+part_sum = sum(tem_list[:K])
+result_list = [part_sum]
+
+for i in range(0, len(tem_list)-K):
+    part_sum = part_sum - tem_list[i] + tem_list[i+K]
+    result_list.append(part_sum)
 
 print(max(result_list))
